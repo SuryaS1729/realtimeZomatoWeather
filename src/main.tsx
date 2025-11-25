c9a53470a261caba54ff796bffd25c008b7e8bc8
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-11-25 17:49:57 +0530

    added tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient(); // Placeholder for future use
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+
   </StrictMode>,
 )
