ab7da5b00865b5a756a89731aeb6a59a5c31c1ec
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-11-29 01:48:04 +0530

    removed the key behaviour and changed the mapclick function to rmeove flicker

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,26 +13,26 @@ export default function Map({coords, onMapClick}: Props) {
     const {lat,lon}=coords
   return (
 <MapContainer 
-key={`${coords.lat},${coords.lon}`}
+
 center={[lat, lon]} zoom={5} style={{width: "700px", height: "500px",  }}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-    <MapClick onMapClick={onMapClick}/>
+    <MapClick onMapClick={onMapClick} coords = {coords}/>
   <Marker position={[lat,lon]}/>
 </MapContainer>  )
 }
 
 
-function MapClick({onMapClick}:{onMapClick:(lat:number, lon:number)=>void}){
+function MapClick({onMapClick, coords}:{onMapClick:(lat:number, lon:number)=>void, coords: Coords}){
     const map = useMap()
+    map.panTo([coords.lat,coords.lon])
+
 
     map.on('click', (e)=>{
         const {lat, lng} = e.latlng
-map.panTo([lat,lng])
-
-    onMapClick(lat,lng)
+        onMapClick(lat,lng)
 
     })
 
