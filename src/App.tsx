74521889ea378ba3eaf698947e24b07c88927b0b
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-11-28 18:57:39 +0530

    added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query"
-import { getWeather } from "./api"
 
 import DailyForecast from "./components/cards/DailyForecast"
 import HourlyForecast from "./components/cards/HourlyForecast"
@@ -8,6 +6,7 @@ import AdditionalInfo from "./components/cards/AdditionalInfo"
 import Map from "./components/Map"
 import { useState } from "react"
 import type { Coords } from "./types"
+import LocationDropDown from "./components/dropdowns/LocationDropDown"
 
 
 function App() {
@@ -21,6 +20,7 @@ function App() {
 
   return (
     <div className="flex flex-col gap-8 shadow-md">
+      <LocationDropDown/>
       <Map coords = {coords} onMapClick ={onMapClick}/>
 <CurrentWeather coords = {coords}/>
 <HourlyForecast coords = {coords}/>
---
+++ src/components/dropdowns/LocationDropdown.tsx
@@ -0,0 +1,41 @@
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+type Props = {}
+
+export default function LocationDropDown({}: Props) {
+  return (
+<Select>
+  <SelectTrigger className="w-[180px]">
+    <SelectValue placeholder="Theme" />
+  </SelectTrigger>
+  <SelectContent className="z-1001">
+    <SelectItem value="light">Light</SelectItem>
+    <SelectItem value="dark">Dark</SelectItem>
+    <SelectItem value="system">System</SelectItem>
+  </SelectContent>
+</Select>  )
+}
+
+// the map has a z-index of 1000
+
+const locations = [
+  // Indian Cities
+  "Mumbai",
+  "Delhi",
+  "Bengaluru",
+  "Hyderabad",
+  "Chennai",
+
+  // Foreign Cities
+  "New York",
+  "London",
+  "Tokyo",
+  "Dubai",
+  "Paris"
+];
